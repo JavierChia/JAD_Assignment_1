2fdd415f7366d5c36e0a556342075f23f6d78821
JavierChia <javierchia.22@ichat.sp.edu.sg>
2023-05-22 17:17:18 +0800

    dude

---
+++ Desktop/Y2S1/JAD/Assignment 1/src/main/java/servlet/RegisterUserServlet.java
@@ -0,0 +1,79 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.mindrot.jbcrypt.BCrypt;
+/**
+ * Servlet implementation class RegisterUserServlet
+ */
+@WebServlet("/RegisterUserServlet")
+public class RegisterUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterUserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		
+		String firstName = request.getParameter("firstName");
+		String lastName =  request.getParameter("lastName");
+		String email = request.getParameter("registerEmail");
+		String password = request.getParameter("registerPassword");
+		String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String connURL = "jdbc:mysql://localhost/bookstore_db?user=root&password=root&serverTimezone=UTC";
+			Connection conn = DriverManager.getConnection(connURL);
+			
+			String sql = "INSERT INTO customers (first_name, last_name, email, password) VALUES(?, ?, ?, ?)";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, firstName);
+			statement.setString(2, lastName);
+			statement.setString(3, email);
+			statement.setString(4, hashedPassword);
+			int rowsAffected = statement.executeUpdate();
+
+		    if (rowsAffected == 1) {
+		        response.sendRedirect("/ST0510_JAD_Assignment_1/login.jsp?statusCode=validRegistration&hashedpw="+hashedPassword);
+		    } else {
+		    	response.sendRedirect("/ST0510_JAD_Assignment_1/login.jsp?statusCode=invalidRegistration");
+		    }
+			// Step 7: Close connection
+			conn.close();
+		} catch (Exception e) {
+			out.println("Error: " + e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
